6c8845b8c1fc9b5692bbda70dcb2860ee51dd78b
Orin Fletcher <orin.fletcher@gmail.com>
2020-09-27 03:08:07 -0700

    implements hash+salt with bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -48,15 +50,20 @@ app
   })
   .post(function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+
     User.findOne({ email: username }, function (err, validUser) {
       if (err) {
         console.log(err);
       } else {
         if (validUser) {
-          if (validUser.password === password) {
-            res.render("secrets");
-          }
+          bcrypt.compare(password, validUser.password, function (err, result) {
+            if (result === true) {
+              res.render("secrets");
+            } else {
+              console.log(err);
+            }
+          });
         }
       }
     });
@@ -69,16 +76,18 @@ app
     res.render("register");
   })
   .post(function (req, res) {
-    const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password),
-    });
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash,
+      });
+      newUser.save(function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets");
+        }
+      });
     });
   });
 
